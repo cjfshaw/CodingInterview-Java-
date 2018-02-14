44f32e476b7be8f06606b8e23eec0c7314b060ba
cjfshaw <cjfshaw@gmail.com>
2018-02-14 03:03:09 -0800

    Update Ch2Q1LLRemoveDupes.java

--- src/Ch2Q1LLRemoveDupes.java
+++ src/Ch2Q1LLRemoveDupes.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
 
 public class Ch2Q1LLRemoveDupes {
+    /*Write code to remove duplicates from an unsorted linked list.
+    FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed.
+     */
+
+    public static void removeDupesWithBuffer(LinkedList<String> inputList) {
+        for( int i=0; i<inputList.size(); i++) {
+            for(int j=i+1; j<inputList.size(); j++) {
+                if(inputList.get(i).equals(inputList.get(j))) {
+                    inputList.remove(j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<String>();
+
+        linkedList.add("v");
+        linkedList.add("a");
+        linkedList.add("a");
+        linkedList.add("v");
+        linkedList.add("h");
+        linkedList.add("v");
+        linkedList.add("i");
+        linkedList.add("i");
+
+        System.out.println(linkedList);
+
+        removeDupesWithBuffer(linkedList);
+
+        System.out.println(linkedList);
+    }
 }
